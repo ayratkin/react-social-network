b09a2cf44bd8ff0397c6e90ff68acef1ebe9909a
Ayrat <ayrat.klim1@yandex.ru>
2022-11-12 01:44:47 +0300

    refactored AddMessage: use hooks to acces the state

--- src/components/Dialogs/AddMessage/AddMessage.jsx
+++ src/components/Dialogs/AddMessage/AddMessage.jsx
@@ -1,36 +1,57 @@
 // Libraries
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+import {changeMessageTextActionCreator, sendMessageActionCreator} from '../../../redux/dialogs-reducer'
 
 // Component styles
 import style from "./AddMessage.module.css"
 
 const AddMessage = (props) => {
+
+    // state
+    const message = useSelector(state => {
+        const message = state.dialogsPage.messageText
+        return message
+    })
+
+    // dispatch
+    const dispatch = useDispatch()
+
     const myRef = React.createRef();
 
     // Печать текста с сохранением в state
     function changeMessage() {
         let text = myRef.current.value;
-        props.changeMessageText(text);
+        dispatch(changeMessageTextActionCreator(text));
     }
 
     // Отправка сообщения в state
     function sendMessage() {
-        props.sendMessage();
+        dispatch(sendMessageActionCreator())
+    }
+
+    // Создание нового поста по нажатию на Enter
+    const pressEnter = (e) => {
+        if (e.keyCode == 13) {
+            sendMessage();
+            // props.postText = '';
+        }
     }
 
     return (
         <>
             <div className={style.new_message}>
-                <textarea 
-                    ref={myRef} 
-                    placeholder="Write a post..." 
-                    value={props.messageTe}
-                    onChange={ changeMessage }>
-                </textarea>
-                <button onClick={ sendMessage }>Send message</button>
+                <textarea
+                    ref={myRef}
+                    placeholder="Write a post..."
+                    value={message}
+                    onChange={changeMessage}
+                    onKeyDown={pressEnter}/>
+                <button onClick={sendMessage}>Send message</button>
             </div>
         </>
     )
-} 
+}
 
 export default AddMessage;
