62abde5e663013d8b660e4f7a8c2775c2382b570
Ayrat <ayrat.klim1@yandex.ru>
2022-11-24 21:35:25 +0300

    'Users' has become a class component, 'hooks' have been replaced by 'connect'.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,78 +1,96 @@
 // Libraries
 import React from "react";
 import axios from "axios";
+import {connect} from "react-redux";
 // Component styles
 import style from './Users.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {followAC, setUsersAC} from '../../redux/users-reducer';
 import {unFollowAC} from '../../redux/users-reducer';
-
 import manAvatar from '../../../src/img/man-avatar.png'
 
-
-const Users = (props) => {
-
-	const users = useSelector(state => {
-		return state.usersPage.users;
-	})
-
-	const dispatch = useDispatch();
-
-	axios.get('https://social-network.samuraijs.com/api/1.0/users').then(respons => {
-		console.log(respons.data.items)
-	})
-
-	if (users.length === 0) {
-		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(respons => {
-			dispatch(setUsersAC(respons.data.items))
-		})
+let mapStateToProps = (state) => {
+	return {
+		users: state.usersPage.users
 	}
-
-	return (
-		<>
-			<h1>Users:</h1>
-			{
-				users.map((user) => {
-					return (
-						<div className={style.user}>
-							<div className={style.user__follow}>
-
-								{
-									user.photos.small
-										? <img src={user.photos.small} className={style.user_photo}/>
-										: <img src={manAvatar} className={style.user_photo}/>
-								}
-
-								{
-									user.isFollow
-										? <button
-											className={style.user__followBtn}
-											onClick={() => {
-												dispatch(unFollowAC(user.id))
-											}}>unfollow</button>
-										: <button
-											className={style.user__followBtn}
-											onClick={() => {
-												dispatch(followAC(user.id))
-											}}>follow</button>
-								}
-
-							</div>
-							<div className={style.user__descriptions}>
-								<div className={style.user__column}>
-									<p className={style.user__fullName}>{user.name}</p>
-									<p className={style.user__status}>{user.status}</p>
-								</div>
-								<div className={style.user__column}>
-									{user.country}
-								</div>
-							</div>
-						</div>
-					)
-				})
-			}
-		</>
-	)
 }
 
-export default Users;
+let mapDispatchToProps = (dispatch) => {
+	return {
+		setUsers: (users) => {
+			dispatch(setUsersAC(users))
+		},
+
+		followUser: (userId) => {
+			dispatch(followAC(userId))
+		},
+
+		unFollowUser: (userId) => {
+			dispatch(unFollowAC(userId))
+		},
+	}
+}
+
+class Users extends React.Component {
+
+	// get info from api
+	getUsers = () => {
+		if (this.props.users.length === 0) {
+			axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
+				respons => {
+					this.props.setUsers(respons.data.items)
+				})
+		}
+	}
+
+	render() {
+		return (
+			<>
+				<h1>Users:</h1>
+				<button onClick={this.getUsers}>Get users</button>
+				{
+					this.props.users.map((user) => {
+						return (
+							<div className={style.user}>
+								<div className={style.user__follow}>
+
+									{
+										user.photos.small
+											? <img src={user.photos.small} className={style.user_photo} alt={'user_photo'}/>
+											: <img src={manAvatar} className={style.user_photo} alt={'user_photo'}/>
+									}
+
+									{
+										user.isFollow
+											? <button
+												className={style.user__followBtn}
+												onClick={() => {
+													this.props.unFollowUser(user.id)
+												}}>unfollow</button>
+											: <button
+												className={style.user__followBtn}
+												onClick={() => {
+													this.props.followUser(user.id)
+												}}>follow</button>
+									}
+
+								</div>
+								<div className={style.user__descriptions}>
+									<div className={style.user__column}>
+										<p className={style.user__fullName}>{user.name}</p>
+										<p className={style.user__status}>{user.status}</p>
+									</div>
+									<div className={style.user__column}>
+										{user.country}
+									</div>
+								</div>
+							</div>
+						)
+					})
+				}
+			</>
+		)
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
