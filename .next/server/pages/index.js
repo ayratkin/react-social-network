cde73d1bb3e623a8038b46df9be585a9520f6163
Ayrat <ayrat.klim1@yandex.ru>
2023-02-05 22:21:38 +0300

    added libraries

---
+++ .next/server/pages/index.js
@@ -0,0 +1,68 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./components/myComp1.module.scss":
+/*!****************************************!*\
+  !*** ./components/myComp1.module.scss ***!
+  \****************************************/
+/***/ ((module) => {
+
+eval("// Exports\nmodule.exports = {\n\t\"aboba\": \"myComp1_aboba__LvYQd\"\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9jb21wb25lbnRzL215Q29tcDEubW9kdWxlLnNjc3MuanMiLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9jb21wb25lbnRzL215Q29tcDEubW9kdWxlLnNjc3M/NTFkZCJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBFeHBvcnRzXG5tb2R1bGUuZXhwb3J0cyA9IHtcblx0XCJhYm9iYVwiOiBcIm15Q29tcDFfYWJvYmFfX0x2WVFkXCJcbn07XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./components/myComp1.module.scss\n");
+
+/***/ }),
+
+/***/ "./components/MyComponent1.jsx":
+/*!*************************************!*\
+  !*** ./components/MyComponent1.jsx ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _components_myComp1_module_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../components/myComp1.module.scss */ \"./components/myComp1.module.scss\");\n/* harmony import */ var _components_myComp1_module_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_components_myComp1_module_scss__WEBPACK_IMPORTED_MODULE_1__);\n\n\nconst MyComponent1 = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        className: (_components_myComp1_module_scss__WEBPACK_IMPORTED_MODULE_1___default().aboba),\n        children: \" MyComponent1 text \"\n    }, void 0, false, {\n        fileName: \"C:\\\\Coding\\\\next-react-social-network\\\\components\\\\MyComponent1.jsx\",\n        lineNumber: 4,\n        columnNumber: 9\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyComponent1);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9jb21wb25lbnRzL015Q29tcG9uZW50MS5qc3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQW9EO0FBRXBELE1BQU1DLGVBQWUsSUFBTTtJQUMxQixxQkFBTyw4REFBQ0M7UUFBSUMsV0FBV0gsOEVBQVk7a0JBQUU7Ozs7OztBQUN0QztBQUVBLGlFQUFlQyxZQUFZQSxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vY29tcG9uZW50cy9NeUNvbXBvbmVudDEuanN4PzA3YjAiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHN0eWxlcyBmcm9tICcvY29tcG9uZW50cy9teUNvbXAxLm1vZHVsZS5zY3NzJ1xyXG5cclxuY29uc3QgTXlDb21wb25lbnQxID0gKCkgPT4ge1xyXG5cdHJldHVybiA8ZGl2IGNsYXNzTmFtZT17c3R5bGVzLmFib2JhfT4gTXlDb21wb25lbnQxIHRleHQgPC9kaXY+XHJcbn1cclxuXHJcbmV4cG9ydCBkZWZhdWx0IE15Q29tcG9uZW50MTsiXSwibmFtZXMiOlsic3R5bGVzIiwiTXlDb21wb25lbnQxIiwiZGl2IiwiY2xhc3NOYW1lIiwiYWJvYmEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./components/MyComponent1.jsx\n");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _components_MyComponent1__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../components/MyComponent1 */ \"./components/MyComponent1.jsx\");\n\n\nconst Profile = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"main page +\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Coding\\\\next-react-social-network\\\\pages\\\\index.js\",\n                lineNumber: 6,\n                columnNumber: 4\n            }, undefined),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_components_MyComponent1__WEBPACK_IMPORTED_MODULE_1__[\"default\"], {}, void 0, false, {\n                fileName: \"C:\\\\Coding\\\\next-react-social-network\\\\pages\\\\index.js\",\n                lineNumber: 8,\n                columnNumber: 4\n            }, undefined)\n        ]\n    }, void 0, true);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Profile);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFvRDtBQUVwRCxNQUFNQyxVQUFVLElBQU07SUFDckIscUJBQ0M7OzBCQUNDLDhEQUFDQzswQkFBRzs7Ozs7OzBCQUVKLDhEQUFDRixnRUFBWUE7Ozs7Ozs7QUFHaEI7QUFFQSxpRUFBZUMsT0FBT0EsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IE15Q29tcG9uZW50MSBmcm9tIFwiL2NvbXBvbmVudHMvTXlDb21wb25lbnQxXCI7XHJcblxyXG5jb25zdCBQcm9maWxlID0gKCkgPT4ge1xyXG5cdHJldHVybiAoXHJcblx0XHQ8PlxyXG5cdFx0XHQ8aDE+bWFpbiBwYWdlICs8L2gxPlxyXG5cclxuXHRcdFx0PE15Q29tcG9uZW50MSAvPlxyXG5cdFx0PC8+XHJcblx0KVxyXG59XHJcblxyXG5leHBvcnQgZGVmYXVsdCBQcm9maWxlOyJdLCJuYW1lcyI6WyJNeUNvbXBvbmVudDEiLCJQcm9maWxlIiwiaDEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ components/MyComponent1.jsx
@@ -0,0 +1,7 @@
+import styles from '/components/myComp1.module.scss'
+
+const MyComponent1 = () => {
+	return <div className={styles.aboba}> MyComponent1 text </div>
+}
+
+export default MyComponent1;
